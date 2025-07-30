5142c938d8d3ed7b8301e1524ff387be65daac83
ethanlim05 <ethanlgy2004@gmail.com>
2025-07-31 01:11:22 +0800

    Removed unused GameConstants class

--- src/main/java/com/example/demo/model/GameConstants.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.model;
-
-public final class GameConstants {
-    public static final double CELL_SIZE = 100.0; // Adjust this value as needed
-    public static final double WINDOW_WIDTH = 900;
-    public static final double WINDOW_HEIGHT = 900;
-    // Add other constants as needed
-}
